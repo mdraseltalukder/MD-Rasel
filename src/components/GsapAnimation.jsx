2325327fe0b1ddf78bc4d677c795fb50941a89ce
Md Rasel <mdrasel551219@gmail.com>
2025-05-30 15:44:51 +0600

    Fix: Animation

--- src/components/GsapAnimation.jsx
+++ src/components/GsapAnimation.jsx
@@ -10,14 +10,14 @@ export default function GsapAnimation({ showPage, setShowPage }) {
           onClick={() => setShowPage(false)}
         >
           <div
-            className="flex flex-col items-center justify-center gap-3"
+            className="flex flex-col items-center justify-center gap-3 mx-auto"
             id="gsap"
           >
             <h2 className="text-4xl font-bold text-white mb-4 cinzel-decorative-regular">
               WELCOME TO MY
             </h2>
 
-            <h1 className="text-5xl font-bold text-[#00e7f8] mb-4">
+            <h1 className="text-5xl font-bold text-center text-[#00e7f8] mb-4">
               PORTFOLIO WEBSITE
             </h1>
           </div>
