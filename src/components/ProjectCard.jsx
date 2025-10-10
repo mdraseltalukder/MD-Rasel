ca4382ec5db18380ccef96710a63709faed6a4a9
Md Rasel <mdrasel551219@gmail.com>
2025-10-10 12:39:58 +0600

    fix: hover and click

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,14 +1,20 @@
 /* eslint-disable react/prop-types */
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const ProjectCard = ({ imgSrc, title, tags, projectLink, description }) => {
+  const [active, setActive] = useState(false);
   return (
     <div className="relative overflow-hidden">
       <figure className="img-box aspect-square rounded-lg  ">
         <img src={imgSrc} alt={title} loading="lazy" className="img-cover " />
       </figure>
 
-      <div className="  project absolute  bottom-0 left-0 bg-[#2c8288e7] inset-0 flex items-center justify-center opacity-0 hover:opacity-100 duration-500 rounded-lg">
+      <div
+        className="  project absolute  bottom-0 left-0 bg-[#2c8288e7] inset-0 flex items-center justify-center opacity-0 hover:opacity-100 duration-500 rounded-lg"
+        style={{ opacity: active ? 1 : undefined }}
+        onClick={() => setActive(!active)}
+      >
         <div className="project-text flex flex-col items-center  gap-4 absolute z-40 inset-x-0 bottom-0 p-4 translate-y-full transition-transform  ease-in-out  overflow-hidden ">
           <h3 className="title-1  text-white">{title}</h3>
           <p className="text-white text-center">{description}</p>
